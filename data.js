5dc8c94d2d31920557669e35890fc1d726fe696b
Carl Durand <carldurand.student@careerdevs.com>
2019-05-20 00:48:56 +0000

    Functional Programming: Learn About Functional Programming

--- data.js
+++ data.js
@@ -1,9 +1,29 @@
-let dog = {
-  name: "Spot",
-  numLegs: 4,
-  sayLegs: function() {
-    return "This dog has " + dog.numLegs + " legs.";
+/**
+ * A long process to prepare tea.
+ * @return {string} A cup of tea.
+ **/
+const prepareTea = () => 'greenTea';
+
+/**
+ * Get given number of cups of tea.
+ * @param {number} numOfCups Number of required cups of tea.
+ * @return {Array<string>} Given amount of tea cups.
+ **/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+  
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
   }
+
+  return teaCups;
 };
 
-dog.sayLegs();
+// Add your code below this line
+
+const tea4TeamFCC = getTea(40); // :(
+
+// Add your code above this line
+
+console.log(tea4TeamFCC);
