f7d87aa2d4dcfe2214e51e2e0ee7a8f0a6d6c0fe
Carl Durand <carldurand.student@careerdevs.com>
2019-05-20 00:36:55 +0000

    Create a React Component

--- react.js
+++ react.js
@@ -1,54 +1,14 @@
-class MyApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      inputValue: event.target.value
-    });
-  }
-  render() {
-    return (
-       <div>
-        { /* change code below this line */ }
-<GetInput input = {this.state.inputValue} handleChange = {this.handleChange}/>
-<RenderInput input = {this.state.inputValue}/>
-        { /* change code above this line */ }
-       </div>
-    );
-  }
-};
-
-class GetInput extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    // change code below this line
     return (
       <div>
-        <h3>Get Input:</h3>
-        <input
-          value={this.props.input}
-          onChange={this.props.handleChange}/>
+       <h1>Hello React!</h1>
       </div>
     );
-  }
-};
-
-class RenderInput extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h3>Input Render:</h3>
-        <p>{this.props.input}</p>
-      </div>
-    );
+    // change code above this line
   }
 };
