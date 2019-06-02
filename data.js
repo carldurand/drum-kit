a2a03d76ed9694026aa6549dcb6e9b1c6330f144
Carl Durand <carldurand.student@careerdevs.com>
2019-06-02 01:25:33 +0000

    Binary Agents

--- data.js
+++ data.js
@@ -1,19 +1,33 @@
-function fearNotLetter(str) {
+function binaryAgent(str) {
+      // Separate the binary code by space.
+      str = str.split(' ');
+      var power;
+      var decValue = 0;
+      var sentence = '';
 
-  for(var i = 0; i < str.length; i++) {
-    /* code of current character */
-    var code = str.charCodeAt(i);
+      // Check each binary number from the array.
+      for (var s = 0; s < str.length; s++) {
+        // Check each bit from binary number
+        for (var t = 0; t < str[s].length; t++) {
+          // This only takes into consideration the active ones.
+          if (str[s][t] == 1) {
+            // This is quivalent to 2 ** position
+            power = Math.pow(2, +str[s].length - t - 1);
+            decValue += power;
 
-    /* if code of current character is not equal to first character + no of iteration
-    hence character has been escaped */
-    if (code !== str.charCodeAt(0) + i) {
+            // Record the decimal value by adding the number to the previous one.
+          }
+        }
 
-      /* if current character has escaped one character find previous char and return */
-      return String.fromCharCode(code - 1);
-    }  
-  }
-  return undefined;
-}
+        // After the binary number is converted to decimal, convert it to string and store
+        sentence += (String.fromCharCode(decValue));
 
-// test here
-fearNotLetter("abce");
+        // Reset decimal value for next binary number.
+        decValue = 0;
+      }
+
+      return sentence;
+    }
+
+    // test here
+    binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
